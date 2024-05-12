02e0b407619f9ac833ba85b64dc19e72c38dcc83
Robert Le <robertle982@gmail.com>
2024-05-11 20:30:30 -0400

    got qis api.py

--- API/API.py
+++ API/API.py
@@ -87,30 +87,30 @@ def create_user():
     phone = cursor.fetchone()
 
     if user or email or phone:
-        return jsonify({'message': 'Username or Email or Phone Number Existed'}), 400
+        return jsonify({'error': 'Username or Email or Phone Number Existed'}), 400
 
     if not validate_username(Username):
-        return jsonify({'message': 'Username must be at least 8 characters(lower case, uppercase or digits) long'}), 400
-
-    if not validate_email(Email):
-        return jsonify({'message': 'Please enter valid email address'}), 400
-
-    if not validate_phone_number(PhoneNumber):
-        return jsonify({'message': 'Please enter valid phone number'}), 400
+        return jsonify({'error': 'Username must be at least 8 characters(lower case, uppercase or digits) long'}), 401
 
     if not validate_password(Password):
-        return jsonify({'message': 'Password must be at least 8 characters(lower case, uppercase or digits) long'}), 400
+        return jsonify({'error': 'Password must be at least 8 characters(lower case, uppercase or digits) long'}), 402
+
+    if not validate_email(Email):
+        return jsonify({'error': 'Please enter valid email address'}), 403
+
+    if not validate_phone_number(PhoneNumber):
+        return jsonify({'error': 'Please enter valid phone number'}), 404
 
     if not all(key in data for key in ('Username', 'PasswordHash')):
-        return jsonify({'error': 'Missing username or password'}), 400
+        return jsonify({'error': 'Missing username or password'}), 405
 
     # Insert into database
     cursor.execute(
         'INSERT INTO Users (Username, PasswordHash, Email, FullName, CurrAddr, PhoneNumber) VALUES (?, ?, ?, ?, ?, ?)',
-        data['Username'], hash_password(Password), data['Email'], data['FullName'], data['CurrAddr'], data['PhoneNumber'])
+        data['Username'], hash_password(Password), data['Email'], data['FullName'], data['CurrentAddress'], data['PhoneNumber'])
     conn.commit()
 
-    return jsonify({'message': 'User created successfully'}), 201
+    return jsonify({'message': 'User created successfully'}), 200
 
 # Check for login
 @app.route('/login', methods=['POST'])
@@ -203,10 +203,9 @@ def open_account():
         # Validate input data
         if not data:
             return jsonify({'error': 'No data provided'}), 400
-        required_fields = ['AccountType']
-        for field in required_fields:
-            if field not in data:
-                return jsonify({'error': f'Missing required field: {field}'}), 400
+        required_field = 'AccountType'
+        if required_field not in data:
+            return jsonify({'error': f'Missing required field: {required_field}'}), 400
 
         # Generate a unique 16-digit Account Number
         account_number = str(uuid.uuid4().int)[:16]
@@ -236,29 +235,41 @@ def open_account():
 @app.route('/myaccounts', methods=['GET'])
 @jwt_required()
 def my_accounts():
-    try:
-        user_id = get_jwt_identity()
+    user_id = get_jwt_identity()
 
-        cursor.execute('SELECT * FROM Accounts WHERE UserID = ?', user_id)
-        accounts = cursor.fetchall()
-        result_list = []
-        if not accounts:
-            return jsonify({'message': 'no account exists'}), 400
-        for account in accounts:
-            account_dict = dict(zip([column[0] for column in cursor.description], account))
+    cursor.execute('SELECT * FROM Accounts WHERE UserID = ?', user_id)
+    accounts = cursor.fetchall()
+    result_list = []
+    if not accounts:
+        return jsonify({'error': 'no account exists'}), 400
+    for account in accounts:
+        account_dict = dict(zip([column[0] for column in cursor.description], account))
 
-            # Convert decimal values to float
-            account_dict['Balance'] = float(account_dict['Balance'])
+        # Convert decimal values to float
+        account_dict['Balance'] = float(account_dict['Balance'])
 
-            # Convert datetime objects to ISO format string
-            account_dict['OpeningDate'] = account_dict['OpeningDate'].isoformat()
+        # Convert datetime objects to ISO format string
+        account_dict['OpeningDate'] = account_dict['OpeningDate'].isoformat()
 
-            result_list.append(account_dict)
-        json_result = json.dumps(result_list)
-        return jsonify(json_result), 200
-    except Exception as e:
-        conn.rollback()
-        return jsonify({'error': str(e)}), 500
+        result_list.append(account_dict)
+    json_result = json.dumps(result_list)
+    return jsonify(json_result), 200
+
+@app.route('/changestatus', methods=['POST'])
+@jwt_required()
+def change_status():
+    data = request.json
+    if not data:
+        return jsonify({'error': 'No data provided'}), 400
+    user_id = get_jwt_identity()
+    account_number = data['AccountNumber']
+    cursor.execute('SELECT * FROM Accounts WHERE UserID = ? AND AccountNumber = ?', user_id, account_number)
+    account = cursor.fetchone()
+    if not account:
+        return jsonify({'error': 'Account not found'}), 404
+    cursor.execute('UPDATE Accounts SET AccStatus = ? WHERE AccountNumber = ?', data['AccountStatus'], data['AccountNumber'])
+    conn.commit()
+    return jsonify({'message': 'Account status updated successfully'}), 200
 
 @app.route('/accoverview', methods=['GET'])
 @jwt_required()
@@ -381,6 +392,100 @@ def get_log_history():
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
+def generate_expiration_date():
+    # Get the current year
+    current_year = datetime.now().year
+
+    # Generate a random year between the current year and the next 10 years
+    expiration_year = random.randint(current_year, current_year + 10)
+
+    # Generate a random month
+    expiration_month = random.randint(1, 12)
+
+    # If the expiration month is February, limit the expiration day to 28
+    if expiration_month == 2:
+        expiration_day = random.randint(1, 28)
+    # For other months, set the maximum day to 30 or 31 depending on the month
+    elif expiration_month in [4, 6, 9, 11]:
+        expiration_day = random.randint(1, 30)
+    else:
+        expiration_day = random.randint(1, 31)
+
+    # Create a datetime object for the expiration date
+    expiration_date = datetime(expiration_year, expiration_month, expiration_day)
+
+    # Format the expiration date as MM/YYYY
+    expiration_date_formatted = expiration_date.strftime("%m-%Y")
+
+    return expiration_date_formatted
+
+def generate_cvv():
+    return ''.join(random.choices('0123456789', k=3))
+
+@app.route('/opencard', methods=['POST'])
+@jwt_required()
+def open_card():
+    data = request.json
+    # Validate input data
+    if not data:
+        return jsonify({'error': 'No data provided'}), 400
+    required_field = 'CardType'
+    if required_field not in data:
+        return jsonify({'error': f'Missing required field: {required_field}'}), 400
+
+    # Generate a unique 16-digit Account Number
+    card_number = str(uuid.uuid4().int)[:16]
+
+    # Get the UserID from the JWT token
+    user_id = get_jwt_identity()
+
+    cursor.execute('SELECT * FROM Users WHERE UserID = ?', user_id)
+    user = cursor.fetchone()
+
+    if not user:
+        return jsonify({'error': 'User not found'}), 404
+
+    # Extract account data
+    card_holder = user[4]
+    expiration = datetime.strptime(generate_expiration_date(), "%m-%Y")
+    cvv = generate_cvv()
+    card_type = data['CardType']
+    opening_date = datetime.now()
+    issued = "AAWCLBank"
+
+    cursor.execute('INSERT INTO Creditcard (CardNumber, CardHolder, ExpirationDate, OpeningDate, Cvv, CardType, IssuedBy, UserID) VALUES (?, ?, ?, ?, ?, ?, ?, ?)'
+                , card_number, card_holder, expiration, opening_date, cvv, card_type, issued, user_id)
+    conn.commit()
+    return jsonify({'message': 'Credit card created successfully'}), 201
+
+@app.route('/mycards', methods=['GET'])
+@jwt_required()
+def my_cards():
+    user_id = get_jwt_identity()
+    cursor.execute('SELECT * FROM Users WHERE UserID = ?', user_id)
+    user = cursor.fetchone()
+    if not user:
+        return jsonify({'error': 'User not found'}), 404
+
+    cursor.execute('SELECT * FROM CreditCard WHERE UserID = ?', user_id)
+    cards = cursor.fetchall()
+    if not cards:
+        return jsonify({'error': 'No cards exist'}), 404
+    result_list = []
+    for card in cards:
+        card_dict = {
+            'CardNumber': card[0],
+            'CardHolder': card[1],
+            'ExpirationDate': card[2],
+            'Cvv': card[4],
+            'CardType': card[5],
+            'Balance': float(card[7]),
+            'CardStatus': card[8]
+        }
+        result_list.append(card_dict)
+    json_result = json.dumps(result_list)
+    return jsonify(json_result), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
